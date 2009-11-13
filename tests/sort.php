6b09807978140aa785cb2991a02874fc62fc074d
simpledom.dev <simpledom.dev@9c183ae8-c17c-11de-8cd0-f941f5a20a08>
2009-11-13 21:58:20 +0000

    Added one test for SimpleDOM::sort() - the method is used in other tests anyway

---
+++ tests/sort.php
@@ -0,0 +1,53 @@
+<?php
+/*
+
+Copyright 2009 The SimpleDOM authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__) . '/../SimpleDOM.php';
+ 
+class SimpleDOM_TestCase_sort extends PHPUnit_Framework_TestCase
+{
+	public function testByAttribute()
+	{
+		$actual = array(
+			new SimpleDOM('<child letter="c" />'),
+			new SimpleDOM('<child letter="a" />'),
+			new SimpleDOM('<child letter="e" />'),
+			new SimpleDOM('<child letter="b" />'),
+			new SimpleDOM('<child letter="d" />')
+		);
+
+		$expected = array(
+			new SimpleDOM('<child letter="a" />'),
+			new SimpleDOM('<child letter="b" />'),
+			new SimpleDOM('<child letter="c" />'),
+			new SimpleDOM('<child letter="d" />'),
+			new SimpleDOM('<child letter="e" />')
+		);
+
+		SimpleDOM::sort($actual, '@letter');
+
+		$this->assertEquals($expected, $actual);
+	}
+}
