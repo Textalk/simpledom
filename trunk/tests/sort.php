0f5b5f2f8751bb2b5f049e1a08050b0a0eff3b26
simpledom.dev <simpledom.dev@9c183ae8-c17c-11de-8cd0-f941f5a20a08>
2009-11-14 06:55:13 +0000

    Added test

--- trunk/tests/sort.php
+++ trunk/tests/sort.php
@@ -38,6 +38,8 @@ class SimpleDOM_TestCase_sort extends PHPUnit_Framework_TestCase
 			new SimpleDOM('<child letter="d" />')
 		);
 
+		SimpleDOM::sort($actual, '@letter');
+
 		$expected = array(
 			new SimpleDOM('<child letter="a" />'),
 			new SimpleDOM('<child letter="b" />'),
@@ -46,7 +48,40 @@ class SimpleDOM_TestCase_sort extends PHPUnit_Framework_TestCase
 			new SimpleDOM('<child letter="e" />')
 		);
 
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testPointersToNodesAreNotLost()
+	{
+		$actual = array(
+			new SimpleDOM('<child letter="c" />'),
+			new SimpleDOM('<child letter="d" />'),
+			new SimpleDOM('<child letter="e" />'),
+			new SimpleDOM('<child letter="a" />'),
+			new SimpleDOM('<child letter="b" />')
+		);
+
+		$c = $actual[0];
+		$d = $actual[1];
+		$e = $actual[2];
+		$a = $actual[3];
+		$b = $actual[4];
+
 		SimpleDOM::sort($actual, '@letter');
+
+		$a['old_letter'] = 'a';
+		$b['old_letter'] = 'b';
+		$c['old_letter'] = 'c';
+		$d['old_letter'] = 'd';
+		$e['old_letter'] = 'e';
+
+		$expected = array(
+			new SimpleDOM('<child letter="a" old_letter="a" />'),
+			new SimpleDOM('<child letter="b" old_letter="b" />'),
+			new SimpleDOM('<child letter="c" old_letter="c" />'),
+			new SimpleDOM('<child letter="d" old_letter="d" />'),
+			new SimpleDOM('<child letter="e" old_letter="e" />')
+		);
 
 		$this->assertEquals($expected, $actual);
 	}
